be6dbe39d372e206e6bc5c5098c16f46c6ed867c
maxmiroff <maxmiroff@gmail.com>
2015-10-27 19:46:10 -0400

    limplemented level editor to mapViewer. Functional and writes to testLevel.level, but fails to write the expected rotation values for each tile.

--- include/Level.hpp
+++ include/Level.hpp
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-#ifndef LEVEL_HPP
-#define LEVEL_HPP
-
-#include <stdio.h>
-#include <vector>
-
-#include "Tile.hpp"
-#include "TileElement.hpp"
-
-class Level {
-    public:
-    
-        std::vector<std::vector<TileElement>> tiles;
-        std::vector<Tile> tileVector;
-        
-        int width;
-        int height;
-        int tileLength; // in pixels
-    
-        // Default constructor
-        
-        // Default destructor
-        
-        bool init(const std::string& levelFilePath, std::list<sf::Vector2f>* meshPoints);
-
-        //Level::export(const std::str filename, std::vector<std::str>* tilefiles)
-    
-    private:
-        bool parseLevel(FILE* levelFile, std::list<sf::Vector2f>* meshPoints);
-};
-#endif
-=======
 #ifndef LEVEL_HPP
 #define LEVEL_HPP
 
@@ -62,4 +29,4 @@ class Level {
         bool parseLevel(FILE* levelFile, std::list<sf::Vector2f>* meshPoints);
 };
 #endif
->>>>>>> 93f87c06fb1a579aa17b78d189646e72a62dc5f2
+
